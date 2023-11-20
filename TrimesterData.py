16e51e6add1f534110994e2b619dc93e4de812e0
Riley LePrell <rileyleprell@gmail.com>
2023-11-20 18:28:26 -0500

    chart_request

--- TrimesterData.py
+++ TrimesterData.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 footballdata = pd.read_csv("FirstTrimesterCCData.csv")
 
 st.dataframe(footballdata)
+
+st.line_chart(data=footballdata, x='QB#', y='J#')
 
